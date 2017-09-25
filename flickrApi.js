7d4e71f27461de486a66347856a15f6af24f23f7
Jayford <jhartford88@gmail.com>
2017-09-24 22:23:14 -0700

    Initalized node-flickr npm package

--- flickrApi.js
+++ flickrApi.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
- 
+ var Flickr = require("node-flickr");
+var keys = { "api_key": "b8cbe7bed4d17de994ee70f68660be56" }
+var flickr = new Flickr(keys);
+
 var checkMandatoryInputs = function(request, response, next){
 
 if(!request.query.query){
